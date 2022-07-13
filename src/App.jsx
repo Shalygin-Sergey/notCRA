279c07937a95cd6bb40a57dbaa1e39926e8fe37f
Шалыгин Сергей <60192169+Shalygin-Sergey@users.noreply.github.com>
2022-07-13 13:44:57 +0300

    Add my proj

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import maks from './img/maks.jpg';
+
+const App = () => {
+    return (
+        <div>
+            <h1>HELLO</h1>
+            <img src={maks} />
+        </div>
+    )
+}
+export default App;
